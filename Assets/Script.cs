457aac6ea3ac404b64960758979b32e0424c8cd0
Cuwor <temichit@yandex.ru>
2018-02-18 10:17:24 +0700

    Start project

---
+++ Assets/Script.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+
+    private Light a;
+    void Start()
+    {
+        a = GetComponent<Light>();
+    }
+    
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	       a.gameObject.SetActive(false);
+	    }
+	}
+}
